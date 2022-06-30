8adba272bd6d8c35bdbbc42e8e2c6f8cb02a4895
drithh <adrielalfeus@student.uns.ac.id>
2022-06-30 22:11:05 +0700

    likelihood

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 const math = require('mathjs');
-const gaussian = require('gaussian');
+
+const input = {
+  Temperature: 20.02222222,
+  JarakPandang: 11.27,
+  Kelembaban: 0.54,
+  KecepatanAngin: 24.6652,
+  DerajatAngin: 359,
+  TekananUdara: 1013.43,
+};
 
 const main = async () => {
   const csvData = [];
-
-  const data = await new Promise((resolve, reject) => {
+  await new Promise((resolve, reject) => {
     fs.createReadStream('test.csv')
       .pipe(csv())
       .on('data', (data) => csvData.push(data))
@@ -15,86 +22,140 @@ const main = async () => {
   });
 
   const dataResult = {
-    TempMax: {},
-    TempMin: {},
-    TempMean: {},
-    WindVeloc: {},
-    Humidity: {},
-    IndeksRadiasiUV: {},
+    Temperature: {},
+    JarakPandang: {},
+    Kelembaban: {},
+    KecepatanAngin: {},
+    DerajatAngin: {},
+    TekananUdara: {},
   };
 
-  const input = {
-    TempMax: 31,
-    TempMin: 21,
-    TempMean: 24.26,
-    WindVeloc: 8,
-    Humidity: 68,
-    IndeksRadiasiUV: 7,
+  const probabilty = {
+    Temperature: {},
+    JarakPandang: {},
+    Kelembaban: {},
+    KecepatanAngin: {},
+    DerajatAngin: {},
+    TekananUdara: {},
+    CurahHujan: {},
   };
 
   Object.keys(dataResult).forEach((key) => {
-    dataResult[key] = clusteringGroup(data, key);
+    dataResult[key] = clusteringGroup(csvData, key);
   });
 
   Object.keys(dataResult).forEach((key) => {
-    console.log(key);
-    getNorm(dataResult[key], input[key]);
+    probabilty[key] = getNormalDistribution(dataResult[key], input[key]);
   });
+  probabilty.CurahHujan = getProbabilty(dataResult.Temperature);
+
+  const result = {
+    likelihood: {},
+    likelihoodNormal: {},
+  };
+  result.likelihood = getLikelihood(probabilty);
+  result.likelihoodNormal = getLikelihoodNormal(result.likelihood);
+  console.log(result);
 };
 
-const getNorm = (data, input) => {
-  // iterate thorub object
+const getLikelihoodNormal = (data) => {
+  const result = {};
+  const sumData = Object.keys(data)
+    .map((key) => {
+      return data[key];
+    })
+    .reduce((a, b) => a + b);
   Object.keys(data).forEach((key) => {
-    console.log(normalDistribution(data[key].mean, data[key].stdDev, input));
+    result[key] = data[key] / sumData;
   });
+
+  return result;
 };
 
-const normalDistribution = (mean, stdDev, input) => {
-  return (
-    (1 / (stdDev * math.sqrt(2 * math.pi))) *
-    math.pow(math.e, (-1 / 2) * math.pow((input - mean) / stdDev, 2))
-  );
+const getLikelihood = (data) => {
+  const result = {
+    clear: 1,
+    foggy: 1,
+    mostlyCloudy: 1,
+    overcast: 1,
+    partlyCloudy: 1,
+  };
+  Object.keys(data).forEach((key) => {
+    Object.keys(data[key]).forEach((key2) => {
+      result[key2] *= data[key][key2];
+    });
+  });
+  return result;
+};
+
+const getProbabilty = (data) => {
+  const groupData = {
+    clear: 0,
+    foggy: 0,
+    mostlyCloudy: 0,
+    overcast: 0,
+    partlyCloudy: 0,
+  };
+  const total = Object.keys(data).map((key) => {
+    return data[key].data.length;
+  });
+  const totalData = math.sum(total);
+  Object.keys(data).forEach((key) => {
+    groupData[key] = data[key].data.length / totalData;
+  });
+  return groupData;
+};
+
+const getNormalDistribution = (data, input) => {
+  const result = {
+    clear: 0,
+    foggy: 0,
+    mostlyCloudy: 0,
+    overcast: 0,
+    partlyCloudy: 0,
+  };
+  Object.keys(data).forEach((key) => {
+    result[key] =
+      (1 / (data[key].stdDev * math.sqrt(2 * math.pi))) *
+      math.pow(
+        math.e,
+        (-1 / 2) * math.pow((input - data[key].mean) / data[key].stdDev, 2)
+      );
+  });
+  return result;
 };
 
 const clusteringGroup = (data, className) => {
-  const groupData = {
-    tidakHujan: [],
-    rendah: [],
-    sedang: [],
-    tinggi: [],
-    sangatTinggi: [],
-  };
   const curahHujan = {
-    tidakHujan: { stdDev: 0, mean: 0 },
-    rendah: { stdDev: 0, mean: 0 },
-    sedang: { stdDev: 0, mean: 0 },
-    tinggi: { stdDev: 0, mean: 0 },
-    sangatTinggi: { stdDev: 0, mean: 0 },
+    clear: { data: [], stdDev: 0, mean: 0 },
+    foggy: { data: [], stdDev: 0, mean: 0 },
+    mostlyCloudy: { data: [], stdDev: 0, mean: 0 },
+    overcast: { data: [], stdDev: 0, mean: 0 },
+    partlyCloudy: { data: [], stdDev: 0, mean: 0 },
   };
   data.forEach((item) => {
-    switch (item.Hujan) {
-      case 'Tidak Hujan':
-        groupData.tidakHujan.push(parseInt(item[className]));
+    switch (item.Cuaca) {
+      case 'Clear':
+        curahHujan.clear.data.push(parseFloat(item[className]));
         break;
-      case 'Rendah':
-        groupData.rendah.push(parseInt(item[className]));
+      case 'Foggy':
+        curahHujan.foggy.data.push(parseFloat(item[className]));
         break;
-      case 'Sedang':
-        groupData.sedang.push(parseInt(item[className]));
+      case 'Mostly Cloudy':
+        curahHujan.mostlyCloudy.data.push(parseFloat(item[className]));
         break;
-      case 'Tinggi':
-        groupData.tinggi.push(parseInt(item[className]));
+      case 'Overcast':
+        curahHujan.overcast.data.push(parseFloat(item[className]));
         break;
-      case 'Sangat Tinggi':
-        groupData.sangatTinggi.push(parseInt(item[className]));
+      case 'Partly Cloudy':
+        curahHujan.partlyCloudy.data.push(parseFloat(item[className]));
         break;
       default:
         break;
     }
   });
-
   Object.keys(curahHujan).forEach((key) => {
-    const data = groupData[key];
+    const data = curahHujan[key].data;
     const mean = math.mean(data);
     const stdDev = math.std(data);
     curahHujan[key].mean = mean;
